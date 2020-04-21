66b5a117d2967ddde7ec2c38bc70d062d1fde336
javierreraul <758906@unizar.es>
2020-04-21 10:51:18 +0200

    Punto 8 totalmente implementado (BIEN)

--- app/src/androidTest/java/es/unizar/eina/notes/NotesTest.java
+++ app/src/androidTest/java/es/unizar/eina/notes/NotesTest.java
@@ -1,30 +1,27 @@
 package es.unizar.eina.notes;
 
-
 import android.database.Cursor;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import es.unizar.eina.bd.NotesDbAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
+import es.unizar.eina.bd.NotesDbAdapter;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class NotesTest {
     @Rule
     public ActivityTestRule<Notes> activityRule = new ActivityTestRule<>(Notes.class);
-    AppCompatActivity Notes;
-
-    long rowIdToDelete;
-
+    private AppCompatActivity Notes;
+    private List<Long> toDelete = new ArrayList<>();
 
     @Before
     public void setUp(){
@@ -34,14 +31,16 @@ public class NotesTest {
     @After
     public void tearDown() {
         NotesDbAdapter mDbHelper = NotesDbAdapter.getNotesDbAdapter(Notes.getApplicationContext());
-        mDbHelper.deleteNote(rowIdToDelete);
+        for( long i : toDelete){
+            mDbHelper.deleteNote(i);
+        }
     }
 
     @Test
     public void testHumo() {
         NotesDbAdapter mDbHelper = NotesDbAdapter.getNotesDbAdapter(Notes.getApplicationContext());
         int pre = mDbHelper.fetchAllNotes().getCount();
-        rowIdToDelete = mDbHelper.createNote("Title","Body", 0, 86400000, "Ninguna");
+        toDelete.add(mDbHelper.createNote("Title","Body", 0, 86400000, "Ninguna"));
         int post = mDbHelper.fetchAllNotes().getCount();
         assertEquals(pre + 1, post);
     }
@@ -50,17 +49,19 @@ public class NotesTest {
     public void testMasExhaustivo(){
         NotesDbAdapter mDbHelper = NotesDbAdapter.getNotesDbAdapter(Notes.getApplicationContext());
         long rowId = mDbHelper.createNote("Title","Body", 0, 86400000, "Ninguna");
+        toDelete.add(rowId);
         Cursor c = mDbHelper.fetchNote(rowId);
         String title = c.getString(c.getColumnIndexOrThrow(NotesDbAdapter.KEY_NOTE_TITLE));
         String body = c.getString(c.getColumnIndexOrThrow(NotesDbAdapter.KEY_NOTE_BODY));
         long activationDate = c.getLong(c.getColumnIndexOrThrow(NotesDbAdapter.KEY_NOTE_ACTIVATION_DATE));
         long expirationDate = c.getLong(c.getColumnIndexOrThrow(NotesDbAdapter.KEY_NOTE_EXPIRATION_DATE));
         String category = c.getString(c.getColumnIndexOrThrow(NotesDbAdapter.KEY_NOTE_CATEGORY));
-        if(title.equals("Title") && body.equals("Body") && activationDate == 0 && expirationDate == 86400 && category.equals("Ninguna")){
+        if(title.equals("Title") && body.equals("Body") && activationDate == 0 && expirationDate == 86400000 && category.equals("Ninguna")){
             assertTrue(true);
         }
         else{
             fail();
         }
+
     }
 }
